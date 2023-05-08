62f91e4623f6710a5ada07ab2c2515ea3a26f1d0
Diego Alpízar <48010731+DiegoAlpizar@users.noreply.github.com>
2023-05-08 23:38:00 +0000

    Solves `as_ref_mut`'s second part... (AsMut)

--- exercises/conversions/as_ref_mut.rs
+++ exercises/conversions/as_ref_mut.rs
@@ -19,9 +19,12 @@ fn char_counter<T: AsRef< str >>(arg: T) -> usize {
 
 // Squares a number using as_mut().
 // TODO: Add the appropriate trait bound.
-fn num_sq<T>(arg: &mut T) {
+fn num_sq<T: AsMut< u32 > >(arg: &mut T) {
+    
     // TODO: Implement the function body.
-    !todo!()
+    let numRef  =   arg.as_mut() ;
+    *numRef    =   numRef.pow( 2 ) ;
+
 }
 
 #[cfg(test)]
